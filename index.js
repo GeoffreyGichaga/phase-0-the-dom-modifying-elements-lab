883a347991d221859ebf820112444dac2f91be4c
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-20 08:18:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const removeMain = document.querySelector('#main')
+
+removeMain.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.textContent = "Jeff is the champion"
+
